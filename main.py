7c168d1d31ea4029f8b712acb414f17bab5ae94e
dtaji <diar.tazhigul@narxoz.kz>
2026-04-07 15:34:48 +0500

    bkb

--- main.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-result = duration[status == "ok"]
-result = df[df["status"] == "ok"]["duration"]
-result = []
-for i in range(len(duration)):
-    if status[i] == "ok":
-        result.append(duration[i])
